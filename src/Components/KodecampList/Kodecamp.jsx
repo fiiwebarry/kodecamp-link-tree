fbcdc7729970655137107969ec317933b241ae41
fiiwebarry <fiiwebarry@yahoo.com>
2023-08-18 13:01:13 +0100

    update

--- src/Components/KodecampList/Kodecamp.jsx
+++ src/Components/KodecampList/Kodecamp.jsx
@@ -8,47 +8,48 @@ import Icon3 from "/src/Assets/Icon/kodeHauz-logo.png";
 import Icon4 from "/src/Assets/Icon/Content.png";
 import Icon5 from "/src/Assets/Icon/Avatar.png";
 
-const KodeCamp=()=>{
+const KodeCamp = () => {
 
 
-    return(
-         <section className="container"> 
-        <img className="displayImagetab" src={Icon4} alt=""/>
-        <img className="displayImage" src={Icon5} alt=""/>
-       <div>
-        <img src={Image} alt=""/>
-        <p className="text-header">John Doe</p>
+   return (
+      <section className="container">
+         <img className="displayImagetab" src={Icon4} alt="" />
+         <img className="displayImage" src={Icon5} alt="" />
+         <div className="John-tab-header">
+            <img src={Image} alt="" />
+            <p className="text-header">John Doe</p>
 
-        </div>
-       <div className="Kodecamp-tab-list">
-          <p className="kodecamp-text-tab">Twitter Link</p>
-       </div>
+         </div>
+         <div className="Kodecamp-tab-list">
+            <p className="kodecamp-text-tab">Twitter Link</p>
+         </div>
 
-       <div className="Kodecamp-tab-list">
-           <p className="kodecamp-text-tab">KodeCamp Team</p>
-       </div>
+         <div className="Kodecamp-tab-list">
+            <p className="kodecamp-text-tab">KodeCamp Team</p>
+         </div>
 
-       <div className="Kodecamp-tab-list">
-         <p className="kodecamp-text-tab">KodeCamp  Books</p>
-       </div>
+         <div className="Kodecamp-tab-list">
+            <p className="kodecamp-text-tab">KodeCamp  Books</p>
+         </div>
 
-       <div className="Kodecamp-tab-list">
-           <p className="kodecamp-text-tab">Background Check for Coders</p>
-        </div>
+         <div className="Kodecamp-tab-list">
+            <p className="kodecamp-text-tab">Background Check for Coders</p>
+         </div>
 
          <div className="Kodecamp-tab-list">
             <p className="kodecamp-text-tab">Design books</p>
          </div>
 
          <div className="Icon-text-tab">
-            <img src={Icon} alt=""/>
-            <img src={Icon1} alt=""/>
+            <img src={Icon} alt="" />
+            <img src={Icon1} alt="" />
          </div>
+         <hr />
 
          <div className="Icon-tab-texter">
-            <img  className="size-tab" src={Icon2} alt=""/>
+            <img className="size-tab" src={Icon2} alt="" />
             <p className="task-tab">KodeCamp Internship Task</p>
-            <img  className="size-tab" src={Icon3} alt=""/>
+            <img className="size-tab" src={Icon3} alt="" />
          </div>
 
 
@@ -58,11 +59,12 @@ const KodeCamp=()=>{
 
 
 
-          </section>
+
+      </section>
 
 
 
-    )
+   )
 }
 
 export default KodeCamp;
